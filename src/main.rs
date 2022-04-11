9a2bf87c73eb65f1d028ab489f654be16a099f19
Phydon <leann.phydon@gmail.com>
2022-04-11 21:32:10 +0200

    added loop and exit request

--- src/main.rs
+++ src/main.rs
@@ -8,69 +8,76 @@ const NUM: usize = 10;
 const NUM_SYM: usize = 10;
 
 fn main() {
-    let lst: [String; NUM] = [
-        "Max".to_string(), 
-        "Günther".to_string(), 
-        "Julia".to_string(), 
-        "Gustav".to_string(), 
-        "Marie".to_string(), 
-        "Uwe".to_string(), 
-        "Luise".to_string(), 
-        "Knut".to_string(), 
-        "Pauline".to_string(), 
-        "Ludwig".to_string()
-    ];
+    loop {
+        let lst: [String; NUM] = [
+            "Max".to_string(), 
+            "Günther".to_string(), 
+            "Julia".to_string(), 
+            "Gustav".to_string(), 
+            "Marie".to_string(), 
+            "Uwe".to_string(), 
+            "Luise".to_string(), 
+            "Knut".to_string(), 
+            "Pauline".to_string(), 
+            "Ludwig".to_string()
+        ];
 
-    let symbols: [char; NUM_SYM] = [
-        char::from_u32(127917).unwrap(),
-        char::from_u32(128520).unwrap(),
-        char::from_u32(128545).unwrap(),
-        char::from_u32(128565).unwrap(),
-        char::from_u32(9875).unwrap(),
-        char::from_u32(9889).unwrap(),
-        char::from_u32(9762).unwrap(),
-        char::from_u32(9763).unwrap(),
-        char::from_u32(9760).unwrap(),
-        char::from_u32(9883).unwrap()
-    ];
+        let symbols: [char; NUM_SYM] = [
+            char::from_u32(127917).unwrap(),
+            char::from_u32(128520).unwrap(),
+            char::from_u32(128545).unwrap(),
+            char::from_u32(128565).unwrap(),
+            char::from_u32(9875).unwrap(),
+            char::from_u32(9889).unwrap(),
+            char::from_u32(9762).unwrap(),
+            char::from_u32(9763).unwrap(),
+            char::from_u32(9760).unwrap(),
+            char::from_u32(9883).unwrap()
+        ];
 
-    let mut n: u64 = 1;
-    for i in 1..50 {
-        exec_clear().expect("Failed to clear screen");
+        let mut n: u64 = 1;
+        for i in 1..50 {
+            exec_clear().expect("Failed to clear screen");
 
-        println!("\\\\ GENERATOR //\n");
-        let name: String = generate(&lst);
-        
-        if i == 49 {
-            generate_symbol(&symbols);
-            println!("{}  <<   Du als nächstes", name.to_uppercase());
-        } else if i < 49 && i > 45 {
-            hourglass(1);
-            println!(">>   {}", name);
-            sleep(n);
-            n += 100;
-        } else if i < 45 && i > 40 {
-            hourglass(2);
-            println!(">>   {}", name);
-            sleep(n);
-            n += 60;
-        } else if i < 40 && i > 33 {
-            hourglass(2);
-            println!(">>   {}", name);
-            sleep(n);
-            n += 30;
-        } else if i < 33 && i > 22 {
-            hourglass(2);
-            println!(">>   {}", name);
-            sleep(n);
-            n += 10;
-        } else if i < 22 {
-            hourglass(2);
-            println!(">>   {}", name);
-            sleep(n);
-            n += 6;
+            println!("\\\\ GENERATOR //\n");
+            let name: String = generate(&lst);
+            
+            if i == 49 {
+                generate_symbol(&symbols);
+                println!("{}  <<   du als nächstes", name.to_uppercase());
+            } else if i < 49 && i > 45 {
+                hourglass(1);
+                println!(">>   {}", name);
+                sleep(n);
+                n += 100;
+            } else if i < 45 && i > 40 {
+                hourglass(2);
+                println!(">>   {}", name);
+                sleep(n);
+                n += 60;
+            } else if i < 40 && i > 33 {
+                hourglass(2);
+                println!(">>   {}", name);
+                sleep(n);
+                n += 30;
+            } else if i < 33 && i > 22 {
+                hourglass(2);
+                println!(">>   {}", name);
+                sleep(n);
+                n += 10;
+            } else if i < 22 {
+                hourglass(2);
+                println!(">>   {}", name);
+                sleep(n);
+                n += 6;
+            }
         }
+
+        let done = quit();
+        if done {break;}
     }
+
+    std::process::exit(0);
 }
 
 fn sleep(num: u64) {
@@ -110,4 +117,33 @@ fn generate_symbol(symbols: &[char; NUM_SYM]) {
     let r = rand::thread_rng().gen_range(1..NUM_SYM);
     let sym =  &symbols[r];
     println!("       {}\n", sym);
+}
+
+fn quit() -> bool {
+    loop {
+        println!("\n\nBeenden? (J/N):");
+
+        let yes: [u8; 1] = *b"J";
+        let no: [u8; 1] = *b"N";
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        let input = input.strip_suffix('\n').unwrap().to_uppercase();
+
+        if input.is_empty() {
+            println!("Bitte \"J\" für Ja eingeben oder \"N\" für Nein.");
+            sleep(1300);
+        } else {
+            let input_bytes = input.as_bytes()[0];
+
+            if input_bytes.eq(&yes[0])  {
+                return true;
+            } else if input_bytes.eq(&no[0]) {
+                return false;
+            } else {
+                println!("Keine gültige Eingabe");
+                println!("Bitte \"J\" für Ja eingeben oder \"N\" für Nein.");
+                sleep(1300);
+            }
+        }
+    }
 }
